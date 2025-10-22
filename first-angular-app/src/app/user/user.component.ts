d72900fceb986a5fbdf1f967ce7215c55a7bf06a
JulioC354R <julyocesarplay@gmail.com>
2025-10-22 19:39:48 -0300

    Recebendo objetos inteiros em componentes

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -7,18 +7,20 @@ import { Component, computed, EventEmitter, input, Input, output, Output } from 
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
 
   //@Output() select = new EventEmitter();
   select = output<string>();
 
   get imagePath() {
-    return '/users/' + this.avatar;
+    return '/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
